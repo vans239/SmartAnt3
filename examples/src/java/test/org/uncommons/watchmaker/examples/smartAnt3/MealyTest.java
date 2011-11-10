0eb6d3e552988d426e05703fd3f08c0fa72fc31b
Evgeny Vanslov <vans239@gmail.com>
2011-11-10 17:09:29 +0400

    structure changes

---
+++ examples/src/java/test/org/uncommons/watchmaker/examples/smartAnt3/MealyTest.java
@@ -0,0 +1,39 @@
+package org.uncommons.watchmaker.examples.smartAnt3;
+/*
+ *  Date: 10.11.11
+ *  Time: 10:24
+ *  Author:
+ *     Vanslov Evgeny
+ *     vans239@gmail.com
+ */
+
+import org.testng.annotations.Test;
+import org.uncommons.watchmaker.examples.smartant3.Mealy.MealyMachine;
+import org.uncommons.watchmaker.examples.smartant3.Mealy.MealyMachineFactory;
+import org.uncommons.watchmaker.examples.smartant3.Mealy.MealyMachineMutation;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class MealyTest {
+    @Test
+    public void testFactory() {
+        Random random = new Random();
+        MealyMachineFactory factory = new MealyMachineFactory(10);
+        MealyMachine machine = factory.generateRandomCandidate(random);
+        assert true;
+    }
+
+    @Test
+    public void testMutation() {
+        Random random = new Random();
+        MealyMachineFactory factory = new MealyMachineFactory(1);
+        MealyMachine machine = factory.generateRandomCandidate(random);
+        List<MealyMachine> list = new ArrayList<MealyMachine>();
+        list.add(machine);
+        MealyMachineMutation mmm = new MealyMachineMutation();
+        List<MealyMachine> result = mmm.apply(list, random);
+        assert true;
+    }
+}
